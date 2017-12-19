2343c59416496ddeb4c5a504f226682b0542beae
fsmgulbahce <m.gulbahce@stu.fsm.edu.tr>
2017-12-19 18:12:26 +0300

    ArabaKiralamaDestanPers

---
+++ src/kullanicilar/Musteri.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kullanicilar;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rosegarden
+ */
+public class Musteri {
+    public long kimlikNo;
+    public String isim;
+    public String sife;
+    public static ArrayList<Musteri> musteriler = new ArrayList<>();
+}
---
+++ src/kullanicilar/Personel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kullanicilar;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rosegarden
+ */
+public class Personel {
+    public String isim;
+    public String sifre;
+    public static ArrayList<Personel> personelListem = new ArrayList<>();
+    
+  
+   
+}
